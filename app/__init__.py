b9ef76a8fdfdf7f96f6a284c82e61a146558b8ee
Leandro Ron Emanuel Aeschbacher <leandro.aeschbacher77@gmail.com>
2025-08-26 22:09:53 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,14 @@
 # app/__init__.py
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from .mqtt_client import mqtt_start, mqtt_stop
+from .config import setup_cors
 from .api import router as api_router
 from .ui import router as ui_router
 from .guests import router as guests_router
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Printer API")
-    app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
-
-    app.include_router(api_router)
+    setup_cors(app)
     app.include_router(ui_router)
     app.include_router(guests_router)
-
-    @app.on_event("startup")
-    async def _startup():
-        mqtt_start()
-
-    @app.on_event("shutdown")
-    async def _shutdown():
-        mqtt_stop()
-
+    app.include_router(api_router)
     return app
