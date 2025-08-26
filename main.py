41a11475cf1a25a9dacb664276a484a605cf984d
Leandro Ron Emanuel Aeschbacher <leandro.aeschbacher77@gmail.com>
2025-08-26 15:38:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# main.py
 from app import create_app
+import os
 
 app = create_app()
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
